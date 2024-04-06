5dec2051c1085591ff673ae8d377268920a17f30
Nordine Bittich <nordine@bittich.be>
2024-04-06 07:32:50 +0200

    sound crackling

--- ubuntu22/start.sh
+++ ubuntu22/start.sh
@@ -4,7 +4,7 @@
 # Function to start VM
 start_vm() {
     qemu-system-x86_64 -enable-kvm -vga virtio -m $1 -smp $2 -cpu host -drive file=vm_disk.img,format=qcow2 \
-        -device intel-hda  \
+        -device ich9-intel-hda -device hda-output\
         -chardev qemu-vdagent,id=ch1,name=vdagent,clipboard=on \
         -device virtio-serial-pci \
         -device virtserialport,chardev=ch1,id=ch1,name=com.redhat.spice.0 \
